032ea27a6edd3195801a3e8c504ae951a6ceac4b
abichen <abinabraham74@gmail.com>
2024-12-22 22:53:54 -0800

    refactoring the add method

--- assignment.rb
+++ assignment.rb
@@ -7,13 +7,28 @@ end
 def add(input_string)
   input_string = input_string.to_s
   numbers = extract_numbers(input_string)
-  negatives = numbers.select(&:negative?)
-  raise NegativeNumbersError, "negative numbers not allowed #{negatives.join(', ')}" unless negatives.empty?
+  return 0 if numbers.empty?
 
-  numbers.sum
+  validate_and_sum_numbers(numbers)
 end
 
 private
+
+def validate_and_sum_numbers(numbers)
+  numbers_sum = 0
+  negatives = []
+
+  numbers.each do |num|
+    negatives << num if num.negative?
+    next unless negatives.empty?
+
+    numbers_sum += num
+  end
+
+  raise NegativeNumbersError, "negative numbers not allowed #{negatives.join(', ')}" unless negatives.empty?
+
+  numbers_sum
+end
 
 def extract_numbers(input_string)
   input_string.to_s
