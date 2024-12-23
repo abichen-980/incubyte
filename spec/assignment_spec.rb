978a458939cdc81b1a1e35a31046caf00fa648e1
abichen <abinabraham74@gmail.com>
2024-12-23 02:16:29 -0800

    rspec organised

--- spec/assignment_spec.rb
+++ spec/assignment_spec.rb
@@ -25,21 +25,69 @@ RSpec.describe '#add' do
     end
 
     it 'should throw an error' do
-      expect { add(test_input) }.to raise_error(NegativeNumbersError, /negative numbers not allowed -5, -3/)
+      expect do
+        add('1,-5,-3')
+      end.to raise_error(NegativeNumbersError, /negative numbers not allowed -5, -3/)
     end
   end
 
-  shared_context 'generic context' do
-    it 'returns 0 for a string with only delimiters' do
-      expect(add(',,,,')).to eq(10)
+  shared_context 'functionality_tests' do
+    context 'should calculate the sum of integers in' do
+      it 'a simple string' do
+        expect(add(input_variants[:simple])).to eq(9)
+      end
+
+      it 'a string with spaces at begining/end' do
+        expect(add(input_variants[:space])).to eq(9)
+      end
+
+      it 'a string with newlines' do
+        expect(add(input_variants[:new_lines])).to eq(9)
+      end
+
+      it 'a string with newtabs' do
+        expect(add(input_variants[:new_tabs])).to eq(9)
+      end
+
+      it 'a string with alphabets' do
+        expect(add(input_variants[:alphabets])).to eq(9)
+      end
+
+      it 'a with special characters' do
+        expect(add(input_variants[:specials])).to eq(9)
+      end
+
+      it 'a with newlines, newtabs, alphabets, special characters' do
+        expect(add(input_variants[:all])).to eq(9)
+      end
+
+      it 'a with multiple delimiters' do
+        expect(add(input_variants[:multiples])).to eq(9)
+      end
+
+      it 'a with only delimiters' do
+        expect(add(input_variants[:only_delimiters])).to eq(0)
+      end
     end
 
+    context 'should raise error for' do
+      it 'a string with only negative numbers' do
+        expect do
+          add(input_variants[:negatives_only])
+        end.to raise_error(NegativeNumbersError, /negative numbers not allowed -1, -5, -3/)
+      end
+
+      it 'a string with some negative numbers' do
+        expect do
+          add(input_variants[:some_negatives])
+        end.to raise_error(NegativeNumbersError, /negative numbers not allowed -5, -3/)
+      end
+    end
+  end
+
+  shared_context 'generic_context' do
     it 'returns 0 for an empty string' do
       expect(add('')).to eq(0)
-    end
-
-    it 'returns 0 for a string with only non-default delimiters' do
-      expect(add('//;;^^^^!;;;')).to eq(0)
     end
 
     it 'returns 0 for a string with no integers' do
@@ -49,111 +97,45 @@ RSpec.describe '#add' do
 
   context 'recieving a string paramater' do
     context 'with default delimiter' do
-      context 'should calculate the sum of integers in' do
-        it 'a simple string' do
-          expect(add('1, 5, 3')).to eq(9)
-        end
-
-        it 'a string with spaces at begining/end' do
-          expect(add('  //,1, 5, 3  ')).to eq(9)
-        end
-
-        it 'a string with newlines' do
-          expect(add("\n1, \n5, \n\n3")).to eq(9)
-        end
-
-        it 'a string with newtabs' do
-          expect(add("\t1, \t5, \t\t3")).to eq(9)
-        end
-
-        it 'a string with alphabets' do
-          expect(add('a1, b5, c3')).to eq(9)
-        end
-
-        it 'a with special characters' do
-          expect(add('#1, %^5, &!3')).to eq(9)
-        end
-
-        it 'a with newlines, newtabs, alphabets, special characters' do
-          expect(add('\n1, \t5, &!b3')).to eq(9)
-        end
-
-        it 'a with multiple delimters' do
-          expect(add('\n1,,,\t5, &!b3')).to eq(9)
-        end
+      let(:input_variants) do
+        {
+          simple: '1, 5, 3',
+          space: '  //,1, 5, 3  ',
+          new_lines: "\n1, \n5, \n\n3",
+          new_tabs: "\t1, \t5, \t\t3",
+          alphabets: 'a1, b5, c3',
+          specials: '#1, %^5, &!3',
+          all: "\n1, \t5, &!b3",
+          multiples: "\n1,,,\t5, &!b3",
+          negatives_only: '-1, -5, -3',
+          some_negatives: "\n1, \t-5, &!b-3",
+          only_delimiters: ',,,,,,,,,, ,,,'
+        }
       end
 
-      context 'should raise error for' do
-        it 'a string with only negative numbers' do
-          expect { add('-1, -5, -3') }.to raise_error(NegativeNumbersError, /negative numbers not allowed -1, -5, -3/)
-        end
-
-        it 'a string with some negative numbers' do
-          expect { add('\n1, \t-5, &!b-3') }.to raise_error(NegativeNumbersError, /negative numbers not allowed -5, -3/)
-        end
-      end
-
-      include_context 'generic context'
+      include_context 'functionality_tests'
+      include_context 'generic_context'
     end
 
-    context 'with a delimter (;)' do
-      context 'should calculate the sum of integers in' do
-        it 'a simple string' do
-          expect(add('//;1; 5; 3')).to eq(9)
-        end
-
-        it 'in a string with space at begining/end' do
-          expect(add('  //;1; 5; 3  ')).to eq(9)
-        end
-
-        it 'in a string with newlines' do
-          expect(add("//;\n1; \n5; \n\n3")).to eq(9)
-        end
-
-        it 'in a string with newtabs' do
-          expect(add("//;\t1; \t5; \t\t3")).to eq(9)
-        end
-
-        it 'in a string with alphabets' do
-          expect(add('//;a1; b5; c3')).to eq(9)
-        end
-
-        it 'a string with special characters' do
-          expect(add('//;#1; %^5; &!3')).to eq(9)
-        end
-
-        it 'a string with newlines, newtabs, alphabets, special characters' do
-          expect(add("//;\n1; \t5; &!b3")).to eq(9)
-        end
-
-        it 'a string with multiple delimters' do
-          expect(add("//;\n1;;;\t5; &!b3")).to eq(9)
-        end
-
-        it 'a string with default delimter' do
-          expect(add("//;\,,,,n1;;;\t5; &!b3")).to eq(9)
-        end
-
-        it 'in a string with empty spaces' do
-          expect(add("//;\     ,n   1;;;\t5; &!b3")).to eq(9)
-        end
+    context 'with delimiter (;)' do
+      let(:input_variants) do
+        {
+          simple: '//;1; 5; 3',
+          space: '  //;1; 5; 3  ',
+          new_lines: "//;\n1; \n5; \n\n3",
+          new_tabs: "//;\t1; \t5; \t\t3",
+          alphabets: '//;a1; b5; c3',
+          specials: '//;#1; %^5; &!3',
+          all: "//;\n1; \t5; &!b3",
+          multiples: "//;\n1;;;\t5; &!b3",
+          negatives_only: '//;-1; -5; -3',
+          some_negatives: "//;\n1; \t-5; &!b-3",
+          only_delimiters: '//;//;;;;;;;;;;'
+        }
       end
 
-      context 'should raise error for' do
-        it 'a string with negative numbers' do
-          expect do
-            add("//;\n1; \t-5; &!b-3")
-          end.to raise_error(NegativeNumbersError, /negative numbers not allowed -5, -3/)
-        end
-
-        it 'a string with only negative numbers' do
-          expect do
-            add('//;-1; -5; -3')
-          end.to raise_error(NegativeNumbersError, /negative numbers not allowed -1, -5, -3/)
-        end
-      end
-
-      include_context 'generic context'
+      include_context 'functionality_tests'
+      include_context 'generic_context'
     end
   end
 end
